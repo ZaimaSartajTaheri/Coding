6838e9cd25ae54088559f1323b2d35e9b1d36c2d
Zaima Sartaj Taheri <67533852+ZaimaSartajTaheri@users.noreply.github.com>
2022-06-30 13:17:53 +0600

    Add files via upload

---
+++ File/Get Current Total Marks of All Students with files/Get Current Total Marks of All Students.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+struct Student{
+char name[100];
+int id;
+int marks;
+
+};
+
+struct Unique_Student{
+char name[100];
+int id;
+int total;
+};
+int main(){
+    FILE *inputFile;
+FILE *outputFile;
+inputFile=fopen("input.txt","r");
+
+if(inputFile==NULL){
+    printf("Not Found");
+    return 0;
+}
+outputFile=fopen("output.txt","w");
+
+     int n;
+     fscanf(inputFile,"%d",&n);
+     struct Student students[n];
+     struct Unique_Student unique_student[n];
+     int k=0;
+     for(int i=0;i<n;i++){
+        fscanf(inputFile,"%s %d %d",&students[i].name,&students[i].id,&students[i].marks);
+        bool found=false;
+
+        for(int j=0;j<k;j++){
+            if(unique_student[j].id==students[i].id){
+                unique_student[j].total+=students[i].marks;
+                found=true;
+            }
+        }
+        if(found==false){
+
+            strcpy(unique_student[k].name,students[i].name);
+            unique_student[k].id=students[i].id;
+            unique_student[k].total=students[i].marks;
+            k++;
+        }
+
+     }
+      for(int i=0;i<k;i++){
+        fprintf(outputFile,"\n%s %d %d\n",unique_student[i].name,unique_student[i].id,unique_student[i].total);
+      }
+     return 0;
+ }
